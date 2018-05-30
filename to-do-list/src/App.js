1d5b1281b15a4e5df51773cb8c5b07a1369cafb3
ashleynicole472 <asha.4720@gmail.com>
2018-05-30 16:42:25 -0700

    Added Bootstrap and deciding layout of page

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -1,19 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import AUX from './hoc/AUX';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <AUX>
+            <div>
+                <h1>My To Do List</h1>
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control" placeholder="Enter your todo item here" aria-label="Recipient's username" aria-describedby="basic-addon2" />
+                <div class="input-group-append">
+                    <button class="btn btn-outline-secondary" type="button">Enter</button>
+                </div>
+                </div>
+                
+                <div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">Cras justo odio</li>
+                        <li class="list-group-item">Dapibus ac facilisis in</li>
+                        <li class="list-group-item">Morbi leo risus</li>
+                        <li class="list-group-item">Porta ac consectetur ac</li>
+                        <li class="list-group-item">Vestibulum at eros</li>
+                    </ul>
+                </div>
+            </div>
+         </AUX>
     );
   }
 }
