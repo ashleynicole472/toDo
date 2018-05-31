e99ace3ef6bb8555bb5a31f8705393d3ef7217c8
ashleynicole472 <asha.4720@gmail.com>
2018-05-30 20:03:41 -0700

    Added bootstrap

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -3,29 +3,49 @@ import './App.css'
 import AUX from './hoc/AUX';
 
 class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            todoItem: []
+        }
+        this.addToDoItemHandler = this.addToDoItemHandler.bind(this)
+    }
+
+    addToDoItemHandler = (event) => {
+        console.log(event.target.value);
+    }
+
   render() {
     return (
         <AUX>
             <div>
                 <h1>My To Do List</h1>
-                <div class="input-group mb-3">
-                    <input type="text" class="form-control" placeholder="Enter your todo item here" aria-label="Recipient's username" aria-describedby="basic-addon2" />
-                <div class="input-group-append">
-                    <button class="btn btn-outline-secondary" type="button">Enter</button>
+                <div className="input-group mb-3">
+                    <input type="text"
+                        className="form-control"
+                        placeholder="Enter your todo item here"
+                        aria-label="Recipient's username"
+                        aria-describedby="basic-addon2" />
+                <div className="input-group-append">
+                    <input
+                        className="btn btn-outline-secondary"
+                        onClick={this.addToDoItemHandler}
+                        type="button">Enter
+                    </button>
                 </div>
                 </div>
-                
+
                 <div>
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item">Cras justo odio</li>
-                        <li class="list-group-item">Dapibus ac facilisis in</li>
-                        <li class="list-group-item">Morbi leo risus</li>
-                        <li class="list-group-item">Porta ac consectetur ac</li>
-                        <li class="list-group-item">Vestibulum at eros</li>
+                    <ul className="list-group list-group-flush">
+                        <li className="list-group-item">Cras justo odio</li>
+                        <li className="list-group-item">Dapibus ac facilisis in</li>
+                        <li className="list-group-item">Morbi leo risus</li>
+                        <li className="list-group-item">Porta ac consectetur ac</li>
+                        <li className="list-group-item">Vestibulum at eros</li>
                     </ul>
                 </div>
             </div>
-         </AUX>
+        </AUX>
     );
   }
 }
